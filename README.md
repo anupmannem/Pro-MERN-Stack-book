ef47633e69f0b40acf2af91d8cfed1a4d3c7920b
anupmannem <anupmannem@gmail.com>
2019-03-30 10:51:27 +0530

    starter babel setup

---
+++ README.md
@@ -0,0 +1,5 @@
+# babel compile
+
+_babel src --presets react --out-dir static_ compiles a .jsx file to js file to static dir
+_babel src --presets react --out-dir static --watch_ watches for changes in jsx file
+_babel src --presets react,es2015 --out-dir static_ includes capabilities of ES6 or ECMAScript2015 features
---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+var contentNode = document.getElementById("contents");
+
+class IssueList extends React.Component {
+    render() {
+        return <div>This is a placeholder for the issue list.</div>;
+    }
+}
+
+ReactDOM.render(<IssueList />, contentNode);
+// const continents = ["Africa", "America", "Asia", "Australia", "Europe"];
+// const message = continents.map(c => `Hello ${c}!`).join(" ");
+// var component = <h1>Hello World! JSX is being tranformed to JS</h1>;
+// var component = <p>{message}</p>;
